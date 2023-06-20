d631cc66f89da77242796bcedf57c9feccf4ef47
Anna Dontcova <anna.dontcova@doctolib.com>
2023-06-20 19:31:16 +0200

    add base settings

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
-      <h1>Vite + React deployed with Github Pages</h1>
+      <h1>Vite + React deployed with Github Pages. Please work!</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
